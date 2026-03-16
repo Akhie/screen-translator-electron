3960dc317da5d57388824e12630be92d5a62177e
Akhilesh Yadav <akhilesh.yadav200093@gmail.com>
2026-03-16 22:54:58 +0530

    Revert "Code for creating .exe file for the desktop app"
    This reverts commit 1ca852a10b058f131ab1acc92f4073e0d180184e.

--- electron/preload.js
+++ electron/preload.js
@@ -5,6 +5,5 @@ contextBridge.exposeInMainWorld("api", {
   captureArea: (coords) => ipcRenderer.invoke("capture-area", coords),
   translate: (text) => ipcRenderer.invoke("translate-text", text),
   on: (channel, callback) => ipcRenderer.on(channel, callback),
-  send: (channel, data) => ipcRenderer.send(channel, data),
-  setIgnoreMouse: (ignore) => ipcRenderer.send("set-ignore-mouse", ignore),
+  send: (channel, data) => ipcRenderer.send(channel, data)
 });
